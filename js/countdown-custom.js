b4fbabc2b706ddfac2f0a68883340948224d188a
PKS <PKS@localhost>
2020-10-11 18:08:02 +0900

    ㅋㅋㅋ

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2020, 10, 7, 15)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2020, 12-1,7)}); // year, month, date, hour
         });
 });		
 
