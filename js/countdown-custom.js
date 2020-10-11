7716b4e2bdfa13c5319846bb5218e86b68c3539c
PKS <PKS@localhost>
2020-10-11 18:17:48 +0900

    d-day 수

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2020, 10, 7, 15)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2020, 10, 7, 15)}); 
+            // year, month, date, hour 월에서 1빼야함
         });
 });		
 
